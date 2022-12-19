22f05345c82fcf5ea83184f7afd629b1e5a8fe92
Saidmurodjon <83033317+Saidmurodjon@users.noreply.github.com>
2022-12-19 14:44:56 +0500

    router Change

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,6 +1,8 @@
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import Notfound from "../components/error/Notfound";
 import Navbar from "../components/navbar/Navbar";
+import Admin from "../view/admin/Admin";
+import Doctor from "../view/doctor/Doctor";
 import Home from "../view/home/Home";
 import Login from "../view/login/Login";
 
@@ -12,6 +14,9 @@ function Routerd() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/doctor" element={<Doctor />} />
+          <Route path="/admin" element={<Admin />} />
+
           <Route path="*" element={<Notfound />} />
         </Routes>
       </Router>
