4c1395421125aa232ea796558e9eddd879c50731
Saidmurodjon <83033317+Saidmurodjon@users.noreply.github.com>
2022-12-19 21:53:45 +0500

    admin change

--- src/view/admin/Admin.js
+++ src/view/admin/Admin.js
@@ -14,108 +14,188 @@ const Admin = () => {
     phone: "",
     history: [],
   });
+  // const arrForm = [
+  //   { id: 1, name: "pasport", type: "text", value: patient.passport },
+  //   // { id: 2, name: "Ismi", type: "text", value: patient.firstName },
+  // ];
   const changeHandler = (e) => {
     setPatient({ ...patient, [e.target.name]: e.target.value });
   };
-  useEffect(()=>{
-console.log(patient);
-  },[patient.passport])
-//   console.log(patient);
+  useEffect(() => {
+    console.log(patient);
+    // eslint-disable-next-line
+  }, [patient.passport]);
+  //   console.log(patient);
   return (
     <div>
       <div className="w-[1200px] mx-auto">
         <div className="grid grid-cols-3 gap-4">
-          <div className="col-span-2 w-5/6 mx-auto">
-            <h1 className="mx-auto">Bemor qo'shish</h1>
-            {/* <Button name={"Bemor qo'shish"}  styles={`right`}/> */}
+          <div className="col-span-3 w-5/6 mx-auto mt-4">
+              {/* <Button name={"Bemor qo'shish"}  styles={`right`}/> */}
             <form className="" action="" onSubmit={(e) => e.preventDefault()}>
-              <input
-                className="w-full h-[60px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2"
-                type="text"
-                placeholder="passport*"
-                name="passport"
-                value={patient.passport}
-                onChange={changeHandler}
-              />
-              <input
-                className="w-full h-[60px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2 mt-[20px]"
-                type="text"
-                placeholder="Ismi*"
-                name="firstName"
-                value={patient.firstName}
-                onChange={changeHandler}
-              />
-              <input
-                className="w-full h-[60px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2 mt-[20px]"
-                type="text"
-                placeholder="Familiya*"
-                name="lastName"
-                value={patient.lastName}
-                onChange={changeHandler}
-              />
-              <input
-                className="w-full h-[60px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2 mt-[20px]"
-                type="text"
-                name="middleName"
-                placeholder="O*"
-                value={patient.middleName}
-                onChange={changeHandler}
-              />
-              <input
-                className="w-full h-[60px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2 mt-[20px]"
-                type="text"
-                name="region"
-                placeholder="region*"
-                value={patient.region}
-                onChange={changeHandler}
-              />
-              <input
-                className="w-full h-[60px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2 mt-[20px]"
-                type="text"
-                name="country"
-                placeholder="country*"
-                value={patient.country}
-                onChange={changeHandler}
-              />
-              <input
-                className="w-full h-[60px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2 mt-[20px]"
-                type="text"
-                name="street"
-                placeholder="street*"
-                value={patient.street}
-                onChange={changeHandler}
-              />
-              <input
-                className="w-full h-[60px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2 mt-[20px]"
-                type="text"
-                name="age"
-                placeholder="age*"
-                value={patient.age}
-                onChange={changeHandler}
-              />
-              <input
-                className="w-full h-[60px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2 mt-[20px]"
-                type="text"
-                name="phone"
-                placeholder="phone*"
-                value={patient.phone}
-                onChange={changeHandler}
-              />
-              <input
-                className="w-full h-[60px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2 mt-[20px]"
-                type="text"
-                name="tel"
-                placeholder="tel*"
-                value={patient.tel}
-                onChange={changeHandler}
-              />
+              <div className="grid grid-cols-3">
+                <div className="px-2">
+                  <label
+                    htmlFor={"passport"}
+                    className=" cursor-pointer text-xl capitalize"
+                  >
+                    Pasport
+                  </label>
+                  <input
+                    className="w-full h-[50px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2 my-4"
+                    id={"passport"}
+                    type={"text"}
+                    placeholder={"Pasport*"}
+                    name={"passport"}
+                    value={patient.passport}
+                    onChange={changeHandler}
+                  />
+                </div>
+                <div className="px-2">
+                  <label
+                    htmlFor={"firstName"}
+                    className=" cursor-pointer text-xl capitalize"
+                  >
+                    Ismi
+                  </label>
+                  <input
+                    className="w-full h-[50px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2 my-4"
+                    id="firstName"
+                    type="text"
+                    placeholder="Ismi*"
+                    name="firstName"
+                    value={patient.firstName}
+                    onChange={changeHandler}
+                  />
+                </div>
+                <div className="px-2">
+                  <label
+                    htmlFor={"lastName"}
+                    className=" cursor-pointer text-xl capitalize"
+                  >
+                    Familiyasi
+                  </label>
+                  <input
+                    className="w-full h-[50px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2 my-4"
+                    id="lastName"
+                    type="text"
+                    placeholder="Familiya*"
+                    name="lastName"
+                    value={patient.lastName}
+                    onChange={changeHandler}
+                  />
+                </div>
+                <div className="px-2">
+                  <label
+                    htmlFor={"middleName"}
+                    className=" cursor-pointer text-xl capitalize"
+                  >
+                    Sharifi
+                  </label>
+                  <input
+                    className="w-full h-[50px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2 my-4"
+                    id="middleName"
+                    type="text"
+                    name="middleName"
+                    placeholder="Sharifi*"
+                    value={patient.middleName}
+                    onChange={changeHandler}
+                  />
+                </div>
+
+                <div className="px-2">
+                  <label
+                    htmlFor={"region"}
+                    className=" cursor-pointer text-xl capitalize"
+                  >
+                    Viloyat nomi
+                  </label>
+                  <input
+                    className="w-full h-[50px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2 my-4"
+                    id="region"
+                    type="text"
+                    name="region"
+                    placeholder="Viloyat*"
+                    value={patient.region}
+                    onChange={changeHandler}
+                  />
+                </div>
+                <div className="px-2">
+                  <label
+                    htmlFor={"country"}
+                    className=" cursor-pointer text-xl capitalize"
+                  >
+                    Tuman nomi
+                  </label>
+                  <input
+                    className="w-full h-[50px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2 my-4"
+                    id="country"
+                    type="text"
+                    name="country"
+                    placeholder="Tuman nomi*"
+                    value={patient.country}
+                    onChange={changeHandler}
+                  />
+                </div>
+                <div className="px-2">
+                  <label
+                    htmlFor={"street"}
+                    className=" cursor-pointer text-xl capitalize"
+                  >
+                    Ko'cha/uy
+                  </label>
+                  <input
+                    className="w-full h-[50px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2 my-4"
+                    id="street"
+                    type="text"
+                    name="street"
+                    placeholder="Ko'cha/uy*"
+                    value={patient.street}
+                    onChange={changeHandler}
+                  />
+                </div>
+                <div className="px-2">
+                  <label
+                    htmlFor={"age"}
+                    className=" cursor-pointer text-xl capitalize"
+                  >
+                    Yosh
+                  </label>
+                  <input
+                    className="w-full h-[50px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2 my-4"
+                    id="age"
+                    type="text"
+                    name="age"
+                    placeholder="Yosh*"
+                    value={patient.age}
+                    onChange={changeHandler}
+                  />
+                </div>
+                <div className="px-2">
+                  <label
+                    htmlFor={"phone"}
+                    className=" cursor-pointer text-xl capitalize"
+                  >
+                    Telefon Raqam
+                  </label>
+                  <input
+                    className="w-full h-[50px] p-2.5 outline-none rounded-md dark:text-[#5B5B5B] border-2 my-4"
+                    id="age"
+                    type="text"
+                    name="phone"
+                    placeholder="+9989"
+                    value={patient.phone}
+                    onChange={changeHandler}
+                  />
+                </div>
+              </div>
             </form>
             <Button
               // ButtonFunction={type === "order" ? SendOrder : SendContact}
               name={"Bemor qo'shish"}
             />
           </div>
-          <div className="">05</div>
+          {/* <div className="">05</div> */}
         </div>
       </div>
     </div>
